49f05e904b6195b083f984d525c447c426c5bcb5
LYNX0007 <136361346+ANSHRAJCODE@users.noreply.github.com>
2026-04-23 16:01:56 +0530

    Delete test_models.py

--- test_models.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-API_KEY = "AIzaSyB50o_tSTOnbjwHPk5nD7VdjEh67aQ2hjs"
-
-client = genai.Client(api_key=API_KEY)
-
-for model in client.models.list():
-    print(model.name)
